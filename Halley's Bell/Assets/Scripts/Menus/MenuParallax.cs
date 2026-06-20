bc3740b6bc0ba1ffa0aae47fd1f82afb939f2755
unknown <mbmartin@ualberta.ca>
2026-06-19 20:30:15 -0600

    gcc hotfixes (menu bg, buttons, parallax, audio. Credits)

--- Halley's Bell/Assets/Scripts/Menus/MenuParallax.cs
+++ Halley's Bell/Assets/Scripts/Menus/MenuParallax.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class MenuParallax : MonoBehaviour
 {
-    Vector2 pz;
     Vector3 StartPos;
 
     [SerializeField] float moveModifier;
@@ -11,15 +10,30 @@ public class MenuParallax : MonoBehaviour
     void Start()
     {
         StartPos = transform.position;
+
+        // This fixes the music and parallax breaking on reload.
+        Time.timeScale = 1f;  // resumes timescale
+        AudioListener.pause = false; // resumes all audiosources
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Camera.main == null)
+        {
+            Debug.LogError("Main camera not found!");
+            return;
+        }
+
+        //Debug.Log("here");
         Vector2 pz = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
         float posX = Mathf.Lerp(transform.position.x, StartPos.x + (pz.x * moveModifier), 2f * Time.deltaTime);
         float posY = Mathf.Lerp(transform.position.y, StartPos.y + (pz.y * moveModifier), 2f * Time.deltaTime);
+
+
+        //Debug.Log(posX);
 
         transform.position = new Vector3 (posX, posY, StartPos.z);
     }
